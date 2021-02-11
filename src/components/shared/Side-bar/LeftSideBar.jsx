2ca89af87ea6300bae90e5e8b45325c8b07b1a38
= <=>
2021-02-11 04:49:56 +0000

    load user information

--- src/components/shared/Side-bar/LeftSideBar.jsx
+++ src/components/shared/Side-bar/LeftSideBar.jsx
@@ -1,59 +1,75 @@
-import React from 'react';
-import {Card, List} from 'antd';
+import React, { useEffect, useState } from 'react'
+import { Card, List } from 'antd'
 
-const {Meta} = Card;
-
+const { Meta } = Card
 
 const data = [
-    {
-        title: 'Ant Design Title 1',
-    },
-    {
-        title: 'Ant Design Title 2',
-    },
-    {
-        title: 'Ant Design Title 3',
-    },
-    {
-        title: 'Ant Design Title 4',
-    },
-];
+  {
+    title: 'Ant Design Title 1',
+  },
+  {
+    title: 'Ant Design Title 2',
+  },
+  {
+    title: 'Ant Design Title 3',
+  },
+  {
+    title: 'Ant Design Title 4',
+  },
+]
 
-function LeftSideBar(props) {
-    return (
-        <>
-            <Card
-                className="mt-3"
-                hoverable
-                cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"/>}
-            >
-                <Meta
-                    title="Europe Street beat"
-                    description={
-                        <ul>
-                            <li>status : Etudaint</li>
-                            <li>email : email@gmail.com</li>
-                            <li>class : GI 5 éme</li>
-                            <li>anneé : 2021</li>
-                        </ul>
-                    }
-                />
-            </Card>
-            <List
-                itemLayout="horizontal"
-                dataSource={data}
-                renderItem={item => (
-                    <List.Item>
-                        <List.Item.Meta
-                            /*   avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"/>}*/
-                            title={<a href="https://ant.design">{item.title}</a>}
-                            description="Ant Design, a design "
-                        />
-                    </List.Item>
-                )}
+const LeftSideBar = (props) => {
+  const [user, setUser] = useState()
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const info = JSON.parse(window.localStorage.getItem('info'))
+    setUser(info.user)
+    setLoading(false)
+  }, [])
+
+  return loading ? (
+    <div>loading</div>
+  ) : (
+    <div>
+      <Card
+        className="mt-3"
+        hoverable
+        cover={
+          <img
+            alt="example"
+            src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+          />
+        }
+      >
+        <Meta
+          title={`${user.email}`}
+          description={
+            <div>
+              <p>Nom: {user.lastName}</p>
+              <p>Prenom: {user.firstName}</p>
+              <p>Role : {user.roles}</p>
+              <p>Creation: {user.createdDate}</p>
+              <p>anneé : 2020/2021</p>
+            </div>
+          }
+        />
+      </Card>
+      <List
+        itemLayout="horizontal"
+        dataSource={data}
+        renderItem={(item) => (
+          <List.Item>
+            <List.Item.Meta
+              /*   avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"/>}*/
+              title={<a href="https://ant.design">{item.title}</a>}
+              description="Ant Design, a design "
             />
-        </>
-    );
+          </List.Item>
+        )}
+      />
+    </div>
+  )
 }
 
-export default LeftSideBar;
+export default LeftSideBar
